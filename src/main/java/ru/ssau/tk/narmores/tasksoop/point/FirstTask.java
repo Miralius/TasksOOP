3a13f14376c8e08245bd844929b19ac3b08f2a9e
Miralius <f-miralius@yandex.ru>
2019-09-15 15:06:55 +0400

    Finished task #1.3

---
+++ src/main/java/ru/ssau/tk/narmores/tasksoop/point/FirstTask.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.narmores.tasksoop.point;
+
+public class FirstTask {
+
+    public static void main(String[] args) {
+        Point origin = new Point(0, 0, 0);
+        Point firstPoint = new Point(3, 4, 5);
+        Point secondPoint = new Point(74.38, 827.88, -4827.7);
+    }
+
+}
---
+++ src/main/java/ru/ssau/tk/narmores/tasksoop/point/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.narmores.tasksoop.point;
+
+public class Point {
+    public final double x, y, z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
