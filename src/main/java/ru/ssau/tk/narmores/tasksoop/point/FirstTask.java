70d35557230cb30c535effc90d6001534a32119f
Miralius <f-miralius@yandex.ru>
2019-09-15 15:38:45 +0400

    Finished task #1.4 and added sout in #1.3

--- src/main/java/ru/ssau/tk/narmores/tasksoop/point/FirstTask.java
+++ src/main/java/ru/ssau/tk/narmores/tasksoop/point/FirstTask.java
@@ -6,6 +6,9 @@ public class FirstTask {
         Point origin = new Point(0, 0, 0);
         Point firstPoint = new Point(3, 4, 5);
         Point secondPoint = new Point(74.38, 827.88, -4827.7);
-    }
 
+        System.out.println("Origin: " + origin.x + ' ' + origin.y + ' ' + origin.z);
+        System.out.println("First point: " + firstPoint.x + ' ' + firstPoint.y + ' ' + firstPoint.z);
+        System.out.println("Second point: " + secondPoint.x + ' ' + secondPoint.y + ' ' + secondPoint.z);
+    }
 }
