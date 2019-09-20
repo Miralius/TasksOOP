e07a31967be0cb664ddf31cb7f8e04ae478fb503
Miralius <f-miralius@yandex.ru>
2019-09-20 20:34:35 +0400

    Finished #1.10 task

---
+++ src/test/java/ru/ssau/tk/narmores/tasksoop/person/PersonTest.java
@@ -0,0 +1,27 @@
+package ru.ssau.tk.narmores.tasksoop.person;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void testMethod() {
+        Person firstObj = new Person();
+        Person secondObj = new Person();
+        firstObj.setFirstName("Anton");
+        secondObj.setFirstName("Ivan");
+        firstObj.setLastName("Ivanov");
+        secondObj.setLastName("Antonov");
+        firstObj.setPassportId(135792);
+        secondObj.setPassportId(864209);
+        assertEquals(firstObj.getFirstName(), "Anton");
+        assertEquals(secondObj.getFirstName(), "Ivan");
+        assertEquals(firstObj.getLastName(), "Ivanov");
+        assertEquals(secondObj.getLastName(), "Antonov");
+        assertEquals(firstObj.getPassportId(), 135792);
+        assertEquals(secondObj.getPassportId(), 864209);
+    }
+
+}
