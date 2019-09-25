6522edbcac3a08195058fbccd04edfb6dfd3f5dc
Frolov <f-miralius@yandex.ru>
2019-09-25 18:29:44 +0400

    Finished #1.11 task

--- src/test/java/ru/ssau/tk/narmores/tasksoop/person/PersonTest.java
+++ src/test/java/ru/ssau/tk/narmores/tasksoop/person/PersonTest.java
@@ -6,6 +6,7 @@ import static org.testng.Assert.*;
 
 public class PersonTest {
 
+    //Task 1.10
     @Test
     public void testMethod() {
         Person firstObj = new Person();
@@ -24,4 +25,42 @@ public class PersonTest {
         assertEquals(secondObj.getPassportId(), 864209);
     }
 
+    //Task 1.11
+
+
+    Person firstObj = new Person();
+    Person secondObj = new Person("Natalia", "Pavlova");
+    Person thirdObj = new Person(null, null, 123456);
+    Person fourthObj = new Person(123456);
+
+    @Test
+    public void testFirstNameMethods() {
+        firstObj.setFirstName("Anton");
+        secondObj.setFirstName(null);
+        thirdObj.setFirstName("Anna");
+        assertEquals(firstObj.getFirstName(), "Anton");
+        assertNull(secondObj.getFirstName());
+        assertEquals(thirdObj.getFirstName(), "Anna");
+        assertNull(fourthObj.getFirstName());
+    }
+
+    @Test
+    public void testLastNameMethods() {
+        thirdObj.setLastName("Frolova");
+        fourthObj.setLastName(null);
+        assertNull(firstObj.getLastName());
+        assertEquals(secondObj.getLastName(), "Pavlova");
+        assertEquals(thirdObj.getLastName(), "Frolova");
+        assertNull(fourthObj.getLastName());
+    }
+
+    @Test
+    public void testPassportIDMethods() {
+        secondObj.setPassportId(742782);
+        fourthObj.setPassportId(697583);
+        assertEquals(firstObj.getPassportId(), 0);
+        assertEquals(secondObj.getPassportId(), 742782);
+        assertEquals(thirdObj.getPassportId(), 123456);
+        assertEquals(fourthObj.getPassportId(), 697583);
+    }
 }
