e9b8e3d21bc933e5eb9de02fd361270bcad1f959
Frolov <f-miralius@yandex.ru>
2019-12-09 00:46:17 +0400

    Finished #1.14 task

--- src/main/java/ru/ssau/tk/narmores/tasksoop/person/Checking.java
+++ src/main/java/ru/ssau/tk/narmores/tasksoop/person/Checking.java
@@ -4,12 +4,19 @@ public class Checking {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-        System.out.println(number);
+        checkAnotherPerson(person);
         System.out.println(person.getFirstName());
     }
+
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
+        System.out.println(person.getFirstName());
+    }
+
+    /*1.13 задание: Сначала выводится Игнат, т.к. создается новый объект и присвается ссылке person внутри метода. После
+    * завершения метода измененная ссылка вместе с новым объектом убивается сборщиком мусора и в main-методе выводится
+    * Arkadiy, определённый в main*/
 
     private static void checkInt(int number) {
         number = 10;
@@ -20,7 +27,7 @@ public class Checking {
         person.setFirstName("Oleg");
         System.out.println(person.getFirstName());
     }
-    /*Сначала выполняется метод checkInt() и выводится число 10, несмотря на то, что мы передаем в этот метод 5.
+    /*1.12 задание: Сначала выполняется метод checkInt() и выводится число 10, несмотря на то, что мы передаем в этот метод 5.
      * Это происходит из-за того, что аргументы-примитивы передаются по значению и их можно редактировать, что внутри метода и
      * сделано (number = 10). Аналогично с методом checkPerson — только Person это ссылочный тип, в аргументе передается
      * ссылка на объект person. Внутри main выдаст 5, т.к. копия с number = 10 осталась в методе checkInt, а имя выведет
