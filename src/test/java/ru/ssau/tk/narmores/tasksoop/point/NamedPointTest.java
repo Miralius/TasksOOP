8824e2ddaf3fafa9e618507f46c75ddc5a1184e6
Frolov <f-miralius@yandex.ru>
2019-12-15 20:09:22 +0400

    Finished #1.16 task

---
+++ src/test/java/ru/ssau/tk/narmores/tasksoop/point/NamedPointTest.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.narmores.tasksoop.point;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class NamedPointTest {
+    NamedPoint unnamedPoint = new NamedPoint(3., 4., 5.);
+    NamedPoint firstPoint = new NamedPoint(3., 4., 7., "First");
+    NamedPoint origin = new NamedPoint();
+
+    @Test
+    public void testGetName() {
+        assertNull(unnamedPoint.getName());
+        assertEquals(firstPoint.getName(), "First");
+        assertEquals(origin.getName(), "Origin");
+    }
+
+    @Test
+    public void testSetName() {
+        unnamedPoint.setName("Second");
+        firstPoint.setName("Third");
+        origin.setName("First");
+        assertEquals(unnamedPoint.getName(), "Second");
+        assertEquals(firstPoint.getName(), "Third");
+        assertEquals(origin.getName(), "First");
+    }
+}
