01ea361611fa591953572e3e5ba364e23153833f
Frolov <f-miralius@yandex.ru>
2019-12-27 00:59:35 +0400

    1.19

---
+++ src/test/java/ru/ssau/tk/narmores/tasksoop/GetNameOfClassOrPrimitiveTest.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.narmores.tasksoop;
+
+import org.testng.annotations.Test;
+import ru.ssau.tk.narmores.tasksoop.person.Person;
+
+import static org.testng.Assert.*;
+
+public class GetNameOfClassOrPrimitiveTest {
+
+    @Test
+    public void testPrintType() {
+        GetNameOfClassOrPrimitive.printType((byte) 255);
+        GetNameOfClassOrPrimitive.printType('z');
+        GetNameOfClassOrPrimitive.printType((short) 65535);
+        GetNameOfClassOrPrimitive.printType(125);
+        GetNameOfClassOrPrimitive.printType(125L);
+        GetNameOfClassOrPrimitive.printType(0.1f);
+        GetNameOfClassOrPrimitive.printType(0.1d);
+        GetNameOfClassOrPrimitive.printType(false);
+        GetNameOfClassOrPrimitive.printType(new Person());
+        GetNameOfClassOrPrimitive.printType(null);
+    }
+
+}
